0f589e2e14d193a63373226647d499fb45c19483
Manuel <mangonzas96@gmail.com>
2017-03-13 18:13:37 +0100

    Migracion paciente y persona

---
+++ database/migrations/2017_03_06_175046_create_personas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    protected $fillable = ['nombre', 'apellido', 'dni', 'telefono', 'correo', 'direccion'];
+
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('dni') -> unique();
+            $table->string('telefono') -> unique();
+            $table->string('correo')-> unique();
+            $table->string('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
